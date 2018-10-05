eb32035c6e68998d67708189cc0b3edd531724f7
Luis Barbosa <luis.barbosa@mymis365.onmicrosoft.com>
2018-10-05 22:16:51 +0100

    Added client

--- gRPCCacheService.Client/Program.cs
+++ gRPCCacheService.Client/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+using static System.Console;
+using static gRPCCaheService.Protos.CacheService;
+using gRPCCaheService.Protos;
 
 namespace gRPCCacheService.Client
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = new Channel("localhost", 5000, ChannelCredentials.Insecure);
+            await channel.ConnectAsync();
+
+            var client = new CacheServiceClient(channel);
+            var response = client.SetAsync(new SetRequest { Key = "ClientDemo", Value = "ClientDemo" });
+
+            ReadLine();
+
+            await channel.ShutdownAsync();
         }
     }
 }
